236cbca7b696f3c63638ea169bc371e886cb6c4f
12anuja <anujapriya96@gmail.com>
2019-07-24 16:00:08 +0530

    Task 4

--- src/main/java/com/stackroute/MuzixApp/controller/MuzixController.java
+++ src/main/java/com/stackroute/MuzixApp/controller/MuzixController.java
@@ -2,15 +2,17 @@ package com.stackroute.MuzixApp.controller;
 
 import com.stackroute.MuzixApp.domain.Muzix;
 import com.stackroute.MuzixApp.service.MuzixSrevice;
-import org.aspectj.weaver.patterns.HasMemberTypePatternForPerThisMatching;
+import io.swagger.annotations.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1")
+@Api(value = "Muzix Management System")
 public class MuzixController
 {
 
@@ -24,8 +26,10 @@ public class MuzixController
 
 
         //Method to save a row in database
+        @ApiOperation(value = "Add a Muzix track")
         @PostMapping("/save")
-        public ResponseEntity<?> saveTrack(@RequestBody Muzix muzix)
+        public ResponseEntity<?> saveTrack(@ApiParam(value = "Muzix object stored in database table", required = true)
+                                           @Valid @RequestBody Muzix muzix)
         {
             ResponseEntity responseEntity;
             try
@@ -43,7 +47,14 @@ public class MuzixController
         }
 
         //Method to display all records in table
-        @GetMapping("/get")
+        @ApiOperation(value = "View a list of available Muzix objects", response = ResponseEntity.class)
+       @ApiResponses(value = {
+                @ApiResponse(code = 200, message = "Successfully retrieved list"),
+                @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+                @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+                @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+        })
+        @GetMapping(value = "/get")
         public ResponseEntity<?> getAllTrack()
         {
             return new ResponseEntity<List<Muzix>>(muzixSrevice.getAllTrack(), HttpStatus.OK);
@@ -51,8 +62,10 @@ public class MuzixController
 
 
         //Method to delete a particular row marked by its ID
+        @ApiOperation(value = "Delete a Muzix object")
         @DeleteMapping("/delete/{trackId}")
-        public ResponseEntity<?> deleteTrack(@PathVariable int trackId)
+        public ResponseEntity<?> deleteTrack(@ApiParam(value = "trackId for which Muzix object will delete from database table", required = true)
+                                                    @PathVariable int trackId)
         {
             try {
                 muzixSrevice.deleteTrack(trackId);
@@ -66,8 +79,12 @@ public class MuzixController
 
 
         //Method to update a row identified by its ID
+        @ApiOperation(value = "Update a Muzix object")
         @PutMapping("/update/{trackId}")
-        public ResponseEntity<?> updateTrack(@PathVariable int trackId, Muzix muzix)
+        public ResponseEntity<?> updateTrack( @ApiParam(value = "Muzix Id to update Muzix object", required = true)
+                                                      @PathVariable(value = "id") int trackId,
+                                              @ApiParam(value = "Update Muzix object", required = true)
+                                              @Valid @RequestBody Muzix muzix)
         {
             ResponseEntity responseEntity;
             try
@@ -85,15 +102,19 @@ public class MuzixController
         }
 
         //Method to display a row based on its trackName
-        @GetMapping("/byName/{trackName}")
-        public ResponseEntity<?> findByName(@PathVariable String trackName, Muzix muzix)
+        @GetMapping(value = "/byName/{trackName}")
+        public ResponseEntity<List<Muzix>> findByName(@ApiParam(value = "trackName for which Muzix object will display from " +
+                                                        "database table", required = true)
+                                                    @PathVariable String trackName,Muzix muzix)
         {
             ResponseEntity responseEntity;
-            try
+          try
             {
                 List<Muzix> findName = muzixSrevice.findByName(trackName);
+               // return new ResponseEntity<List<Muzix>>(findName,HttpStatus.OK);
                 muzixSrevice.saveTrack(muzix);
-                if(findName != null) {
+
+               if(findName != null) {
                     responseEntity = new ResponseEntity<String>("Found track", HttpStatus.FOUND);
                 }else
                 {
--- src/main/java/com/stackroute/MuzixApp/domain/Muzix.java
+++ src/main/java/com/stackroute/MuzixApp/domain/Muzix.java
@@ -1,5 +1,7 @@
 package com.stackroute.MuzixApp.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,13 +18,18 @@ import javax.persistence.Id;
 @NoArgsConstructor
 //Marked to create a parameterized constructor for the class
 @AllArgsConstructor
+@ApiModel(description = "All details about the Muzix tracks")
 public class Muzix
 {
 
     //Marked as the primary key in database table
     @Id
+    @ApiModelProperty(notes = "The database generated ID")
     private int trackId;
+    @ApiModelProperty(notes = "The Muzix track name")
     private String trackName;
+    @ApiModelProperty(notes = "The Muzix track comment")
     private String comment;
+
 
 }
